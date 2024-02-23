b243fd3a6f847f8b113329b3aa52caf088e071fc
Liam Fruzyna <liam@fruzyna.net>
2024-02-23 12:18:44 -0600

    Added Pinty's Series and Modified Tour, date handling improvements

--- fetch.py
+++ fetch.py
@@ -27,6 +27,10 @@ class Series(object):
             self.races = process_imsa(self.schedule_url, self)
         elif 'arcaracing.com' in self.schedule_url:
             self.races = process_arca(self.schedule_url, self)
+        elif 'nascar.ca' in self.schedule_url:
+            self.races = process_nascar_ca(self.schedule_url, self)
+        elif 'nascar.com' in self.schedule_url and 'modified' in self.schedule_url:
+            self.races = process_nascar_mod(self.schedule_url, self)
 
 
 class Race(object):
@@ -47,19 +51,55 @@ class Race(object):
 
 
 series = [
+    Series('NPS', 'https://www.nascar.ca/schedule/', ['NASCAR', 'Stock']),
+    Series('NWMT', 'https://www.nascar.com/nascar-whelen-modified-tour-schedule/', ['NASCAR', 'Stock', 'Open-Wheel']),
     Series('NCS', 'https://www.espn.com/racing/schedule', ['NASCAR', 'Stock', 'Premier']),
     Series('NXS', 'https://www.espn.com/racing/schedule/_/series/xfinity', ['NASCAR', 'Stock']),
     Series('NCTS', 'https://www.espn.com/racing/schedule/_/series/camping', ['NASCAR', 'Stock']),
     Series('ARCA', 'https://www.arcaracing.com/2024-race-broadcast-schedule/', ['Stock']),
     Series('INDY', 'https://www.espn.com/racing/schedule/_/series/indycar', ['IndyCar', 'Open-Wheel', 'Premier']),
     Series('NXT', 'https://www.indycar.com/INDYNXT/Schedule', ['IndyCar', 'Open-Wheel']),
-    #Series('F1', 'https://www.espn.com/f1/schedule', ['Grand-Prix', 'Open-Wheel', 'Premier']),
-    Series('F1', 'https://www.espn.com/racing/schedule/_/series/f1', ['Grand-Prix', 'Open-Wheel', 'Premier']),
+    Series('F1', 'https://www.espn.com/f1/schedule', ['Grand-Prix', 'Open-Wheel', 'Premier']),
+    #Series('F1', 'https://www.espn.com/racing/schedule/_/series/f1', ['Grand-Prix', 'Open-Wheel', 'Premier']),
     Series('WTSC', 'https://www.imsa.com/weathertech/tv-streaming-schedule/', ['IMSA', 'GT', 'Prototype', 'Premier']),
     Series('PILOT', 'https://www.imsa.com/michelinpilotchallenge/tv-streaming-schedule/', ['IMSA', 'GT', 'Touring'])
 ]
 
 YEAR = now = datetime.now().year
+TIME_FORMAT = '%I:%M %p'
+
+
+def get_date_format(short_month=False, include_weekday=True, short_weekday=False):
+    """Builds a the most common date format strings."""
+    weekday = ''
+    if (include_weekday):
+        weekday = '%a, ' if short_weekday else '%A, '
+
+    month = '%b' if short_month else '%B'
+    return f'%Y {weekday}{month} %d'
+
+
+def scrub_date(date_str):
+    """Remove unnecessary information from a date string, replaces unknown times with noon."""
+    return date_str.replace('.', '').replace(' ET', '').replace('Noon', '12:00 PM').replace('TBA', '12:00 PM').replace('TBD', '12:00 PM')
+
+
+def parse_date(date_str, short_month=False, include_weekday=True, short_weekday=False, date_separator='', eastern_time=True):
+    """Takes an un-scrubbed date string and returns a time in central time."""
+    date_str = f'{YEAR} {scrub_date(date_str)}'
+    date_format = get_date_format(short_month, include_weekday, short_weekday)
+    time_format = TIME_FORMAT
+    if ':' not in date_str:
+        time_format = '%I %p'
+
+    if date_separator:
+        date_separator += ' '
+
+    dt = datetime.strptime(date_str, f'{date_format} {date_separator}{time_format}')
+    if eastern_time:
+        dt = dt - timedelta(hours=1)
+
+    return dt
 
 
 def process_espn_racing(url: str, series: Series) -> list:
@@ -82,13 +122,9 @@ def process_espn_racing(url: str, series: Series) -> list:
                 if date:
                     date += ' '
                 date += s
-            date = date.replace('Noon', '12:00 PM')
-            if date != 'DATE':
-                # use noon if time is not yet set
-                if date.endswith('TBD'):
-                    date = date.replace('TBD', '12:00 PM ET')
 
-                dt = datetime.strptime(f'{YEAR} {date.replace(".", "")}', '%Y %a, %b %d %I:%M %p ET')
+            if date != 'DATE':
+                dt = parse_date(date, short_month=True, short_weekday=True)
 
                 # use track as race name
                 race = ''
@@ -98,15 +134,11 @@ def process_espn_racing(url: str, series: Series) -> list:
                         race = s
                     # interpret postponed dates
                     elif s.startswith("**Race postponed to "):
-                        date = s[s.index(' to ')+4:]
-                        dt = datetime.strptime(f'{YEAR} {date}', '%Y %B %d at %I:%M %p')
+                        dt = parse_date(s[s.index(' to ')+4:], short_month=True, include_weekday=False, date_separator='at')
                     elif 'Practice' in s or 'Qualifying' in s or 'Shootout' in s:
                         skip = True
                     elif 'Sprint' in s:
                         race += ' (Sprint)'
-
-                # remove an hour for central time
-                dt = dt - timedelta(hours=1)
 
                 # remove annoying extract cup series text
                 if race.startswith('NASCAR') and ' at ' in race:
@@ -148,8 +180,7 @@ def process_espn_f1(url: str, series: Series) -> list:
         # interpret date time
         date = cells[2].string
         if " - " in date:
-            dt = datetime.strptime(f'{YEAR} {date}', '%Y %b %d - %I:%M %p')
-            dt = dt - timedelta(hours=1)
+            dt = parse_date(date, short_month=True, include_weekday=False, date_separator='-')
 
             # interpret race name
             race = ''
@@ -183,8 +214,8 @@ def process_imsa(url: str, series: Series) -> list:
         name = row.find('a', class_='onTv-event-title').string.strip()
         name = name.replace(' (Only Available To Stream In The United States On Peacock Premium)', '')
         name = name.replace(' (Available Globally)', '')
-        date = row.find("span", class_='date-display-single').string.split(' -')[0]
-        dt = datetime.strptime(date, '%A, %B %d, %Y – %I:%M %p')
+        date = scrub_date(row.find("span", class_='date-display-single').string.split(' -')[0])
+        dt = datetime.strptime(date, f'%A, %B %d, %Y – {TIME_FORMAT}')
         dt = dt - timedelta(hours=1)
 
         # determine TV channel by image
@@ -234,14 +265,10 @@ def process_indy(url: str, series: Series) -> list:
         date = item.find("div", class_='schedule-list__date')
         month = date.contents[0].strip()
         day = date.find('span', class_='schedule-list__date-day').string.strip()
-        time = item.find('span', class_='timeEst').string.replace('ET', '').strip()
-        # use noon if time is not yet set
-        if time == 'TBD':
-            dt = datetime.strptime(f"{month} {day} {YEAR} 12:00 PM", '%b %d %Y %I:%M %p')
-        else:
-            dt = datetime.strptime(f"{month} {day} {YEAR} {time}", '%b %d %Y %I:%M %p')
+        time = item.find('span', class_='timeEst').string.strip()
+        date = scrub_date(f'{YEAR} {month} {day} {time}')
 
-        dt = dt - timedelta(hours=1)
+        dt = parse_date(f'{month} {day} {time}', short_month=True, include_weekday=False)
 
         # determine TV channel by image
         tvimg = item.find("div", class_='schedule-list__broadcast-logos').a['href'].upper()
@@ -278,20 +305,12 @@ def process_arca(url: str, series: Series) -> list:
             # combine date and time, then interpret
             date = cells[0].string.replace('Sept', 'Sep')
             time = cells[3].string.replace('*', '')
-            if time == 'TBA':
-                time = '12:00 pm ET'
+            date = f'{date} {time}'
 
-            if ':' not in time:
-                time = time.replace(' p.m.', ':00 pm')
-
-            date = f'{date} {time}'.replace('.', '')
             try:
-                dt = datetime.strptime(f'{YEAR} {date}', '%Y %A, %b %d %I:%M %p ET')
+                dt = parse_date(date, short_month=True)
             except ValueError:
-                dt = datetime.strptime(f'{YEAR} {date}', '%Y %A, %B %d %I:%M %p ET')
-
-            # remove an hour for central time
-            dt = dt - timedelta(hours=1)
+                dt = parse_date(date)
 
             # use track as race name
             race = cells[1].string
@@ -305,6 +324,68 @@ def process_arca(url: str, series: Series) -> list:
 
             if tv != stream and stream != 'Fox Sports App':
                 races[-1].channel += ' ' + stream.replace(' / Fox Sports App', '')
+
+    return races
+
+
+def process_nascar_ca(url: str, series: Series) -> list:
+    """Fetch race schedule from arcaracing.com"""
+    races = []
+
+    # get rows of table
+    req = Request(url, headers=HEADERS)
+    page = urlopen(req)
+    html = page.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
+    rows = soup.table.find_all("tr")
+    rows.pop(0)
+
+    for row in rows:
+        cells = row.find_all("td")
+        if len(cells) >= 5:
+            # combine date and time, then interpret
+            date = cells[1].find('div', 'event-date').string
+            time = cells[1].find('div', 'event-time').string
+
+            dt = parse_date(f'{date} {time}', short_month=True)
+
+            # use track as race name
+            race = cells[0].find('div', 'race-name').string
+
+            races.append(Race(race, series, dt, 'FloRacing'))
+
+    return races
+
+
+def process_nascar_mod(url: str, series: Series) -> list:
+    """Fetch race schedule from arcaracing.com"""
+    races = []
+
+    # get rows of table
+    req = Request(url, headers=HEADERS)
+    page = urlopen(req)
+    html = page.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
+    rows = soup.table.find_all("tr")
+    rows.pop(0)
+
+    for row in rows:
+        cells = row.find_all("td")
+        if len(cells) >= 5:
+            # combine date and time, then interpret
+            date = cells[1].contents[0].string.strip()
+            time = cells[1].find('p', 'race-time').string   
+            date = f'{date} {time}'
+
+            try:
+                dt = parse_date(date)
+            except ValueError:
+                dt = parse_date(date.replace('Sept', 'Sep'), short_month=True)
+
+            # use track as race name
+            race = cells[0].find('span', 'race-name-span').string
+
+            races.append(Race(race, series, dt, 'FloRacing'))
 
     return races
 
