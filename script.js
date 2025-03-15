aa404897c9c6260b5283ff2a030546bb0e85964b
Liam Fruzyna <liam@fruzyna.net>
2025-03-15 08:23:10 -0500

    Fixed date conversion JS (for daylight time)

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 /**
+ * Ensures there is a leading zero on any single digit datetime values.
+ * @param {Number} number Datetime number to pad.
+ */
+function pad(number)
+{
+    return number.toString().padStart(2, '0')
+}
+
+/**
  * Updates the times to the current timezone and highlights live races.
  */
 function update_time()
@@ -11,7 +20,8 @@ function update_time()
         // read time from table
         let date = el.getElementsByClassName('date')[0]
         let time = el.getElementsByClassName('time')[0]
-        let local = new Date(`${now.getFullYear()}/${date.innerText} ${time.innerText} CST`)
+        // TODO: determine CDT/CST automatically
+        let local = new Date(`${now.getFullYear()}/${date.innerText} ${time.innerText} CDT`)
 
         // determine if local time matches server time zone
         if (update_time === undefined)
@@ -23,8 +33,8 @@ function update_time()
         // update time if timezone differs
         if (update_time)
         {
-            date.innerText = `${local.getMonth()}/${local.getDate()}`
-            time.innerText = `${local.getHours()}:${local.getMinutes().padStart(2, '0')}`
+            date.innerText = `${pad(local.getMonth() + 1)}/${pad(local.getDate())}`
+            time.innerText = `${pad(local.getHours())}:${pad(local.getMinutes())}`
         }
 
         // highlight races starting within an hour or started within the last 3 hours
